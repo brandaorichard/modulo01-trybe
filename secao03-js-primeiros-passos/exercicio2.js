f7443011131089ab546eeaae3ccb350896b06250
Ricardo <rbrandao98@hotmail.com>
2023-03-30 23:35:56 -0300

    Primeiros passos JS

---
+++ secao03-js-primeiros-passos/exercicio2.js
@@ -0,0 +1,10 @@
+// 2. if/esle para que retorne o maior de dois números. Definir duas variaveis para comparar os valores.
+
+let a = 14;
+let b = 20;
+
+if (a > b) {
+    console.log("'a' e maior que 'b'");
+}   else {
+    console.log("'b' e maior que 'a'");
+}
---
+++ secao03-js-primeiros-passos/js-exercicio.js
@@ -0,0 +1,10 @@
+// Exercicio 1: Operacoes aritmeticas
+
+let a = 10;
+let b = 5;
+
+console.log("Soma: " + (a + b));
+console.log("Subtracao: " + (a - b));
+console.log("Multiplicacao: " + (a * b));
+console.log("Divisao: " + (a / b));
+console.log("Modulo: " + (a % b));
